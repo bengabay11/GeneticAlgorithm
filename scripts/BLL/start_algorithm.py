081af61130fdac10198b2a2bf760ee08a41ecebe
ben <ben.gabay38@gmail.com>
2019-06-13 21:44:23 +0300

    added license file

--- scripts/BLL/start_algorithm.py
+++
@@ -1,8 +0,0 @@
-import config
-from scripts.BLL.population import Population
-
-
-def start_algorithm():
-    target_chromosome = get_image_values()
-    population = Population(config.POPULATION_SIZE, target_chromosome)
-    population.sort_chromosomes()
